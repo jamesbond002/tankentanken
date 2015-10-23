e02f409b5c87129b3c4abc49307c2222aa63117a
Fred <jamesbond00700@yandex.com>
2015-10-23 15:23:55 +0200

    First try!

---
+++ plugin.js
@@ -0,0 +1,14 @@
+{
+"moduleId" : "jamesbond002/tankentanken",
+"title" : "tankentanken",
+"subtitle" : "www.tankentanken.de",
+"backButton" : true,
+"enableGPS" : true,
+"zoomControl" : false,
+"screenLockRot" : false,
+"reload" : false,
+"shouldOverrideUrlLoading1" : "http://www.tankentanken.de",
+"shouldOverrideUrlLoading2" : "https://www.tankentanken.de",
+"loadUrl" : "http://www.tankentanken.de/suche/diesel/5/Aktuelle%20Position/#lat#/#lng#",
+"browserLaunchLink" : "http://www.tankentanken.de/suche/diesel/5/Aktuelle%20Position/#lat#/#lng#"
+}
